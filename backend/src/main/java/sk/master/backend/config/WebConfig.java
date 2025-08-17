8340a8dae0bc686718e4c0282e4c9a03a4a4ddf6
Peter Cerven <peter.cerven33@gmail.com>
2025-08-17 18:08:51 +0200

    feat[backend]: basic configuration

---
+++ backend/src/main/java/sk/master/backend/config/WebConfig.java
@@ -0,0 +1,27 @@
+package sk.master.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.public.url}")
+    private String publicFrontendUrl;
+
+    @Value("${frontend.private.url}")
+    private String privateFrontendUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/public/**")
+                .allowedOrigins(publicFrontendUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+
+        registry.addMapping("/api/private/**")
+                .allowedOrigins(privateFrontendUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
